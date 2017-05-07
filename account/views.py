fb054e8569e09a54f705d16ec0979668fa96c101
Ettiboy <ettiboy@RENTLPriyo-MAC.local>
2017-05-08 02:16:15 +0530

    added forms and setup new account login app

--- account/views.py
+++ account/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from account.forms import LoginForm
+
 # Create your views here.
+def account_login(request):
+    context = {
+        login_form: LoginForm
+    }
